002ee8fafe2cd8e9d4c2f987451769dc42ce8338
HafizahmadAziz <hahmadaziz@gmail.com>
2019-04-23 11:20:35 +0500

    1 commit

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Toast.makeText ( this, "first commit", Toast.LENGTH_SHORT ).show ();
+        Toast.makeText ( this, "first commit", Toast.LENGTH_SHORT ).show ();
 
     }
 }
